033a2284c21712c0d916ee57ea9429294c59703e
seonyeonghun <kjca1117@naver.com>
2024-11-08 16:16:15 +0900

    todos controller 완성

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -2,6 +2,16 @@ const Todo = require("../models/todoModel")
 
 // 화살표 함수 (ES6)
 const getAllTodos = async (req, res) => {
+    let todos;
+    try {
+       todos = await Todo.find();
+      if (!todos) {
+        throw new Error("등록된 할일이 없습니다.")
+        res.status(501).send("할일이 등록되어 있지 않습니다.")
+      }
+    } catch (error) {
+      console.log("getAllTodos Fail : ", error)
+    }
     res.status(200).json(todos);
   }
 const addTodo = async (req, res) => {
@@ -17,22 +27,35 @@ const addTodo = async (req, res) => {
     res.status(200).send("add Todo");
   }
 const getTodo = async (req, res) => {
-    // 파라미터 변수 : 문자(string) 타입
-    const foundOne = todos.find((todo) => todo.id === parseInt(req.params.id));
-    res.status(200).json(foundOne);
+    const id = req.params.id; // _id 값을 대신하는 
+    let todo;
+    try {
+      todo = await Todo.findById(id)
+    } catch (error) {
+      console.log("getTodo is Fail : ", error)
+    }
+    res.status(200).json(todo);
   }
 const updateTodo = async (req, res) => {
-    const { id, title, completed } = req.body;
-    const foundOne = todos.find((todo) => todo.id === id);
-    // update field
-    foundOne.title = title;
-    foundOne.completed = completed;
-    res.status(200).json(todos);
+    const id = req.params.id;
+    const {title, completed} = req.body;
+    let todo;
+    try {
+      todo = await Todo.findByIdAndUpdate(id, {title, completed});      
+    } catch(error) {
+      console.log("updateTodo is Fail : ", error)
+    }
+    res.status(200).json(todo);
   }
 const removeTodo = async (req, res) => {
-    let { id } = req.body;
-    todos.splice(--id, 1);
-    res.status(200).json(todos);
+    const id = req.params.id;
+    let todo;
+    try {
+      todo = await Todo.deleteOne({_id: id})
+    } catch (error) {
+      console.log("removeTodo is Fail : ", error)
+    }
+    res.status(200).json(todo);
   }
 
 module.exports = {getAllTodos, addTodo, getTodo, updateTodo, removeTodo}
