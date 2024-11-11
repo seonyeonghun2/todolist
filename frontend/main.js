bd6bcd2b0b4212620231879f9c3b6733c192dbe2
seonyeonghun <kjca1117@naver.com>
2024-11-11 17:31:59 +0900

    할일 삭제 기능 및 UI 구현

--- frontend/main.js
+++ frontend/main.js
@@ -36,15 +36,35 @@ async function fetchTodos () {
     success: function (rows) {
       const todoUL = $("#todos");
       let str = ""; // 자바스크립트 변수는 초기화를 하지 않으면 undefined (상태)
-      rows.forEach((row) => {
-        str += `<li class="d-flex gap-1">
+      rows.forEach((row, i) => {
+        str += `<li class="d-flex gap-1" data-id="${row._id}">
             <input type="checkbox" ${row.completed ? "checked" : ""}>
             <p>${row.title}</p>
-            <input type="button" value="modify">
-            <input type="button" value="remove">
+            <input type="button" value="수정" class="modify-btn">
+            <input type="button" value="삭제${i}" class="remove-btn">
         </li>`;
       });
       todoUL.html(str);
+      removeTodo();
     },
   });
+}
+
+function removeTodo() {
+  const removeBtns = $(".remove-btn");
+  removeBtns.on("click", function(){
+    const currBtn = $(this); // 현재 누른, 버튼이 무엇인지
+    $.ajax({
+      url: `http://localhost:3000/todos/${currBtn.parent().attr("data-id")}`,
+      method: "delete",
+      success: function() {
+        alert("선택항목이 삭제되었습니다");        
+      }
+    }).done(() => {
+      fetchTodos(); // 그럼, 다시 전체 할일목록을 가져와
+    })
+  })
+  // [삭제]버튼 누를때 서버에 있는 데이터 삭제 요청
+  // 서버에 있는 데이터 가져오기 (fetch)
+  // 화면에 표시하기
 }
